1ec052ba5b924a98fee74caae4defb1fd5caee9b
FernandoNB <pronegocios13@gmail.com>
2022-09-01 18:10:38 -0300

    products

--- index.php
+++ index.php
@@ -71,8 +71,54 @@
 <!-- third child -->
 
 <div class="bg-light">
+  <h3 class= "text-center">Hidden store</h3>
+  <p class= "text-center">communication is at the heart of e-commerce and comunity</p>
     
 </div>
+
+<!-- fourth child -->
+ <div class="row">
+   <div class="col-md-10">
+    <!-- products -->
+     <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4">
+      <div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4">
+      <div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+     </div>
+</div>
+  <div class="col-md-2"> 
+<!-- sidenav -->
+<h1>hey</h1>
+  </div>
+ </div>
+ 
 
 
 
